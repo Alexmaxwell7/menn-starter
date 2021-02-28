676a9d525e7c1a17776c7b96adfb19af64da62b7
Alexmaxwell7 <Alexmaxwell3096@gmail.com>
2021-02-28 23:12:10 +0530

    fix(jwt-cookie)

--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,27 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">MENN Starter</h1>
-      <nuxt-link to="/todo">Todo-App</nuxt-link>
+  <div>
+    <div v-if="$store.state.auth">
+      <p>
+        You are authenticated. You can see the
+        <NuxtLink to="/secret"> secret page </NuxtLink>!
+      </p>
+      <button @click="logout">Logout</button>
     </div>
+    <p v-else>
+      Please
+      <NuxtLink to="/login"> login </NuxtLink>.
+    </p>
   </div>
 </template>
 
 <script>
+const Cookie = process.client ? require('js-cookie') : undefined
 export default {
-  mounted() {
-    this.$axios.get('/api').then((res) => {
-      // eslint-disable-next-line no-console
-      console.log(res)
-    })
+  methods: {
+    logout() {
+      Cookie.remove('auth')
+      this.$store.commit('setAuth', null)
+    },
   },
 }
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
